1d24bd778464e71b76203bca6c55c7bf57d9d1c4
Gareth Hind <garethjhind@gmail.com>
2025-03-24 20:50:32 +1100

    Feat: About, Contact, & Services

--- nextjs-app/app/components/Section/Hero/Hero.tsx
+++ nextjs-app/app/components/Section/Hero/Hero.tsx
@@ -1,25 +1,27 @@
-import { Grid, Wrapper } from "@/app/styles/Layout.styles";
+import { Grid, Section, Wrapper } from "@/app/styles/Layout.styles";
 import { HeroSection } from "./Hero.styles";
 import Image from "next/image";
 import UnDrawImg from "@/app/assets/img/illustrations/undraw_responsive_6c8s.svg";
+import { Paragraph, PrimaryHeading } from "@/app/styles/Typography.styles";
 
 export default function Hero() {
   return (
-    <HeroSection>
+    <Section>
+      {/* <Grid placeItems="center"> */}
       <Wrapper>
-        <Grid>
+        <Grid rows="2">
           <Image src={UnDrawImg} alt="responsive" width={500} height={350} />
           <section>
-            <h1 className="primary-heading | u-margin-bottom-small">
+            <PrimaryHeading className="u-margin-bottom-small">
               Get scalable, modern and reliable{" "}
               <span className="">custom software</span>.
-            </h1>
+            </PrimaryHeading>
 
-            <p className="paragraph">
+            <Paragraph>
               We develop web, mobile and custom software that&apos;s tailor-made
               to capture all your unique and essential requirements.
-            </p>
-            <p className="paragraph">{/* <CtaBtn /> */}</p>
+            </Paragraph>
+            <Paragraph>{/* <CtaBtn /> */}</Paragraph>
           </section>
         </Grid>
         {/* <div className="container | u-mt-3xl | u-grid u-even-columns u-gap-5">
@@ -46,6 +48,7 @@ export default function Hero() {
           </div>
         </div> */}
       </Wrapper>
-    </HeroSection>
+      {/* </Grid> */}
+    </Section>
   );
 }
