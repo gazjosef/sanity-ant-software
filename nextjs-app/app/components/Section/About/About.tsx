0ee92a306ab020f3b846e20c7d1329733b18d89f
Gareth Hind <garethjhind@gmail.com>
2025-03-26 21:54:35 +1100

    Refactor: About.tsx

--- nextjs-app/app/components/Section/About/About.tsx
+++ nextjs-app/app/components/Section/About/About.tsx
@@ -1,7 +1,7 @@
 import { Grid, Section, Wrapper } from "@/app/styles/Layout.styles";
 // import { HeroSection } from "./Hero.styles";
 import Image from "next/image";
-import UnDrawImg from "@/app/assets/img/illustrations/undraw_responsive_6c8s.svg";
+import UnDrawImg from "@/app/assets/img/illustrations/undraw_developer_activity_bv83.svg";
 import { Paragraph, PrimaryHeading } from "@/app/styles/Typography.styles";
 
 export default function About() {
@@ -17,8 +17,10 @@ export default function About() {
           <Image src={UnDrawImg} alt="responsive" width={500} height={350} />
           <section>
             <Paragraph>
-              We develop web, mobile and custom software that&apos;s tailor-made
-              to capture all your unique and essential requirements.
+              Founded in 2016, Ant Software is a web development company that
+              helps clients meet the challenges of the digital age. We
+              specialize in brand design, web design and development, and custom
+              software.
             </Paragraph>
           </section>
         </Grid>
