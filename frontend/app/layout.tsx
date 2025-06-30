dc5ccb382f4626c856af5f5183d0abe404f2a920
Gareth Hind <garethjhind@gmail.com>
2025-06-30 21:44:24 +1000

    Fix: Header

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -82,7 +82,7 @@ export default async function RootLayout({
           {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
           <SanityLive onError={handleError} />
           <Header2 />
-          <Header />
+          {/* <Header /> */}
           <main className="">{children}</main>
           <Footer />
         </section>
